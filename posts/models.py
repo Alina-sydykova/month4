cfdc5ff4ed830b66d27e7b78f6a13d4b043dcc8b
Alina <sydykovaalina712@gmail.com>
2025-04-12 01:23:12 +0600

    lesson 2

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+"""CREATE TABLE posts(id serial primary key autoincrement, title varchar(256), content varchar(856), rate int);"""
+"""a
+ll objects - SELECT * FROM post ===> Post.objects.all
+filtred objects - SELECT * FROM post WHERE title = ILIKE('title') ===> Post.objects.filter(title_icontains="title")
+one object -SELECT * FROM post WHERE id = 1==> Post.objects.get(id=1)
+"""
+
+class Post(models.Model):
+    title = models.CharField(max_length=356)
+    content = models.CharField(max_length=856)
+    rate = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    update_at = models.DateTimeField(auto_now=True, null=True)
+
+def __str__(self):
+    return f" {self.title} {self.content} self.id"
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, HttpResponse
 import random
+from posts.models import Post
 
 def test_view(requests): 
     return HttpResponse(f"HELLO WORLD {random.randint(1, 100)}")
 
-def html(request):
+def html_view(request):
     return render(request, "base.html")
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "posts/post_list.html", context={"posts": posts})
