efa05d28f2c6454414c785e62c43f62a1ea368ff
Alina <sydykovaalina712@gmail.com>
2025-04-18 22:13:20 +0600

    lesson4

--- posts/models.py
+++ posts/models.py
@@ -1,27 +1,17 @@
 from django.db import models
 
-"""CREATE TABLE posts(id serial primary key autoincrement, title varchar(256), content varchar(856), rate int);"""
-"""a
-ll objects - SELECT * FROM post ===> Post.objects.all
-filtred objects - SELECT * FROM post WHERE title = ILIKE('title') ===> Post.objects.filter(title_icontains="title")
-one object -SELECT * FROM post WHERE id = 1==> Post.objects.get(id=1)
-"""
-
 class Category(models.Model):
     name = models.CharField(max_length=256)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
     
 
 class Tag(models.Model):
     name = models.CharField(max_length=256)
 
     def __str__(self):
-        return f"{self.name}"
-
-
-
+        return self.name
 
 
 class Post(models.Model):
@@ -31,8 +21,8 @@ class Post(models.Model):
     rate = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     update_at = models.DateTimeField(auto_now=True, null=True)
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    Tag = models.ManyToManyField(Tag)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    tags = models.ManyToManyField(Tag)
 
-def __str__(self):
-    return f" {self.title} {self.content} self.id"
+    def __str__(self):
+        return f"{self.id} | {self.title} - {self.content[:30]}"
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render, HttpResponse
 import random
 from posts.models import Post
+from posts.forms import PostForm
+
 
 def test_view(request): 
     return HttpResponse(f"HELLO WORLD {random.randint(1, 100)}")
 
-def html_view(request):
+
+def home_page_view(request):
     return render(request, "base.html")
 
+
 def post_list_view(request):
-    posts = Post.objects.all()
-    return render(request, "posts/post_list.html", context={"posts": posts})
+    if request.method == "GET":
+        posts = Post.objects.all()
+        return render(request, "posts/post_list.html", context={"posts": posts})
+
 
 def post_detail_view(request, post_id):
-    post = Post.objects.get(id=post_id)
-    return render(request, "posts/post_detail.html", contex={"posts":post})
+    if request.method == "GET":
+        post = Post.objects.get(id=post_id)
+      
+        return render(request, "posts/post_detail.html", context={"post": post})
+
+
+def post_create_view(request):
+    if request.method == "GET":
+        form = PostForm()
+        return render(request, "posts/post_create.html", context={"form": form})
+
+    if request.method == "POST":
+       
+        form = PostForm(request.POST, request.FILES)
+        if not form.is_valid():
+            return render(request, "posts/post_create.html", context={"form": form})
+
+        tags = form.cleaned_data.pop("tags")
+        post = Post.objects.create(**form.cleaned_data)
+        post.tags.set(tags)
+        return render(request, "posts/post_detail.html", context={"post": post})
